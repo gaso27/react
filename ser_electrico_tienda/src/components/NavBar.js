79d092eb69a3adb7c1c3ba11cff67afb23c1e94e
gaso27 <gaso27@hotmail.com>
2022-01-25 18:03:47 -0300

    Intento de menu, tira un error

---
+++ ser_electrico_tienda/src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './NavBar.css'
+
+export default function NavBar() {
+    return <>
+        <NavBar />♠
+        <nav>
+            <div>
+                <div class="nav-wrapper">
+                💡<h1>Bienvenidos a Ser-Electrico </h1> 💡
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li>Nosotros</li>
+                        <li>Carrito</li>
+                        <li>Contacto</li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </>
+}
