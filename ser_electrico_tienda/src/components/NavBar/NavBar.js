c781aad56b9d1b14c86210df4f698845b424f2ea
gaso27 <gaso27@hotmail.com>
2022-01-25 22:16:32 -0300

    Ahi quedo bien

--- ser_electrico_tienda/src/components/NavBar/NavBar.js
+++ ser_electrico_tienda/src/components/NavBar/NavBar.js
@@ -11,11 +11,11 @@ const NavBar = () =>{
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Planner</Nav.Link>
+                    <Nav.Link href="#link">Tienda</Nav.Link>
                     <Nav.Link href="#link">Contacto</Nav.Link>
                     <Nav.Link href="#link">Ingresar</Nav.Link>
-                    <NavDropdown title="Curso Online" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Quiero crecer en Insta</NavDropdown.Item>
+                    <NavDropdown title="Tutoriales Online" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Como instalar de 0</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Quiero crear mi pagina Web</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
